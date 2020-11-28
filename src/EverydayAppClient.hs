14cae719848f8906cfb4a92a4a66c4cbc5af91b9
Mladen Srdić <m.s.qubits@gmail.com>
2020-11-28 16:25:05 +0100

    Placeholder functions.

---
+++ src/EverydayAppClient.hs
@@ -0,0 +1,11 @@
+module EverydayAppClient ( mark, skip, unmark ) where
+
+import Data.Text ( Text )
+
+mark :: Text -> Int -> IO ()
+skip :: IO ()
+unmark :: IO ()
+
+mark = undefined
+skip = undefined
+unmark = undefined

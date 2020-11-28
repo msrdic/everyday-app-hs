0d83d975f89d0854c0ea37b9a6a33bb844b74185
Mladen Srdić <m.s.qubits@gmail.com>
2020-11-28 18:01:04 +0100

    Extracted a common function.

--- src/EverydayAppClient.hs
+++ src/EverydayAppClient.hs
@@ -5,7 +5,7 @@ import System.FilePath ((<.>), (</>))
 import Control.Lens ((.~), (&), (^.))
 
 import qualified Data.Configurator as DC
-import Network.Wreq (responseBody, getWith, statusCode, postWith, defaults, header, responseStatus)
+import Network.Wreq (responseBody, getWith, statusCode, postWith, defaults, header, responseStatus, Options)
 import Data.Text.Encoding (encodeUtf8)
 import Data.Maybe (fromJust)
 import Data.Aeson (Result (..), fromJSON, (.:), withObject, parseJSON, FromJSON, object, ToJSON, toJSON, (.=))
@@ -41,25 +41,26 @@ _getAuthToken path = do
 
 _fromResult :: Result a -> a
 _fromResult (Success r) = r
-_fromResult (Error e) = error e  
+_fromResult (Error e) = error e
 
-_habitIds :: IO [Int]
-_habitIds = do
+_getOpts :: IO Options
+_getOpts = do
     token <- _getAuthToken ("conf" </> "auth" <.> "config")
     let authToken = if token == Nothing
                       then error "Authorization token not configured."
                       else fromJust token
     let opts = defaults & header "Authorization" .~ [encodeUtf8 authToken]
+    return opts
+
+_habitIds :: IO [Int]
+_habitIds = do
+    opts <- _getOpts
     resp <- getWith opts "https://api.everyday.app/habits/"
     return $ nub $ sort $ map _id $ V.toList $ V.map _fromResult $ V.map fromJSON $ resp ^. responseBody . _Array
 
 mark d h = do
-    token <- _getAuthToken ("conf" </> "auth" <.> "config")
-    let authToken = if token == Nothing
-                      then error "Authorization token not configured."
-                      else fromJust token
-    let opts = defaults & header "Authorization" .~ [encodeUtf8 authToken]
-        payload = HabitPayload d h
+    opts <- _getOpts
+    let payload = HabitPayload d h
     resp <- postWith opts "https://api.everyday.app/mark" (toJSON payload)
     let status = resp ^. responseStatus . statusCode
     return $ if status == 200 then OK else NotOK
